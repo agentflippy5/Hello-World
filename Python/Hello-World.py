5647fb438c1fa50d8ceb063ebc35a4a6ca23b1b2
agentflippy5 <agentflippy5@gmail.com>
2014-10-24 15:51:24 +0000

    Add Python files

---
+++ Python/Hello-World.py
@@ -0,0 +1,6 @@
+un = INPUT("What is your name?")
+print ("Hi, " + un + "!")
+uos = INPUT("What is your favorite OS to code on?")
+up = INPUT("What is your favorite program to code with?")
+print ("Cool! I like using " + up + " on " + uos + "too!")
+x = INPUT("Press enter to quit.")
