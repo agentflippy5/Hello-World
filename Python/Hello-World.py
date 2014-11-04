599634ff006360d6aae3ddfecebe2692366f5dcc
unknown <agentflippy5@gmail.com>
2014-11-04 17:12:57 -0500

    Fix grammer error

--- Python/Hello-World.py
+++ Python/Hello-World.py
@@ -2,7 +2,7 @@ un = input("What is your name? ")
 print ("Hi, " + un + "!")
 uos = input("What is your favorite OS to code on? ")
 up = input("What is your favorite program to code with?  ")
-print ("Cool! I like using " + up + " on " + uos + "too!")
+print ("Cool! I like using " + up + " on " + uos + " too!")
 if up.lower == "python":
   print ("Did you know this program was written in python?")
 x = input("Press enter to quit.")
